9a8d20f306c292002ccd15ee73680898264c6434
Gopi <gopise73@gmail.com>
2021-12-08 21:05:03 +0530

    additional files and changes for Heroku

--- app/views.py
+++ app/views.py
@@ -3,6 +3,12 @@ from app.models import Contact
 
 # Create your views here.
 
+# git remote add origin https://github.com/Gopipatwa/DJ.git
+# git branch -M main
+# git push -u origin main
+# https://careerkarma.com/blog/git-permission-denied-publickey/:~:text=The%20%E2%80%9CPermission%20denied%20(publickey).%20fatal:%20Could%20not%20read,it%20is%20not,%20add%20your%20key%20to%20Git.
+
+
 
 def home(request):
     con = Contact.objects.all()
